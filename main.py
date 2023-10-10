77eeeb3da777f457d9a03fca136ce3b2abd64880
kenny <64386216+keennneeth@users.noreply.github.com>
2023-10-09 21:26:31 -0500

    update

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import math
 import random as rd
-import sys
-
 
 
 '''
@@ -21,17 +19,16 @@ def inverse_mod_p(prime1, prime2):
     r,s = gcd(prime1, prime2)      #a and p have to be positive numbers
     if r != 1:          # a mod p == r
         flag = False
-        e = rd.randrange(3, s)
     else:
         flag = True
     return s, flag
 
 
 
-#function used to find gcd by solving as+tp=r
+#function used to find greatest common divisor by solving as+tp=r
 
 def gcd(prime1, prime2):
-    oldr, r = (prime1, prime2)
+    oldr, r = (prime1 , prime2 )
     olds, s = (1,0)
     oldt, t = (0,1)
 
@@ -43,48 +40,90 @@ def gcd(prime1, prime2):
    
     if olds < 0:
         olds = olds + prime2
-    return oldr, olds
+    return olds, oldr
 
+
+
+
+#Finding LCM
 def LCM(prime1, prime2):
     np1 = prime1 - 1
     np2 = prime2 - 1
-
-
-
     return (math.lcm(np1, np2))
 
 
 
 if __name__ == '__main__':
-
-        
+ 
     primeslist = "primes.txt"
 
     with open(primeslist) as primes:
         primenumbers = primes.read()
 
-    print("Step 1  - Alice picking two primes 1-500")
-    prime1 = input("Pick your first prime number - ") + "\n"
-    if prime1 in primenumbers:
-        prime1 = int(prime1)
-    else:
-        print("Not a prime number, try again")
-        sys.exit()
+    print("Step 1  - Alice generates two primes 1-500")
+    prime1 = int(rd.choice(open("primes.txt").readlines()))
+    prime2 = int(rd.choice(open("primes.txt").readlines()))
+    print("Prime 1 = ", prime1)
+    print("Prime 2 = ", prime2)
 
-    prime2 = input("Pick your second prime number - ") + "\n"
-    if prime2 in primenumbers:
-        prime2 = int(prime2)
-    else:
-        print("Not a prime number, try again")
-        sys.exit()
-
-print("\n\nStep 2 - LCM of",prime1,"and", prime2)  
-print("λ = (prime1 − 1) (prime2 − 1)")
-print("λ = ", LCM(prime1, prime2))  
+# # print("\nStep 2 - LCM of",prime1,"and", prime2)  
+# # print("LCM = (prime1 − 1) (prime2 − 1)")
+# print("LCM = ", LCM(prime1, prime2))  
 
 
-print("\n\nStep 3 - Inverse multiplicative, (T/F)")  
-print(inverse_mod_p(prime1, prime2))
+# # print("\nGCD of",prime1, prime2,)
+# # print("GCD = (prime1 − 1) (prime2 − 1)")
+# print("inverse Multiplicative = ",gcd(prime1,prime2)[0])
+k = gcd(prime1,prime2)[0]
+N = prime1*prime2
+# print("N = ", N)
+
+mult = (prime1-1) * (prime2 -1)
+# print("λ = ", mult)
+
+e = rd.randint(3, mult - 1)
+
+
+
+# print("e = ", e)
+# print(inverse_mod_p(prime1, prime2))
+
+# # print("λ = (prime1 − 1) (prime2 − 1)/g ")
+# p1 = prime1 -1
+# p2 = prime2 -1
+# m = p1*p2
+# print("λ = ",int(m/gcd(prime1-1,prime2-1)))
+
+
+# print("\nStep 3 - Inverse multiplicative, (T/F)")  
+# print(inverse_mod_p(prime1, prime2))
+
+# print("\nStep 4 - GCD and inverse multiplicative", gcd(prime1, prime2))  
+message = "Secret"
+
+
+print("Public Key = ", e)
+print("Private Key = ", k)
+print("N = ", N)
+print("λ = ", mult)
+print("\n\n")
+print("Message = ", message)
+
+messageord = [ord(l) for l in message]
+
+
+
+cipher = [pow(l, e, N) for l in messageord]
+print(cipher)
+
+
+messageord = [pow(l, k, N) for l in cipher]
+print(messageord)
+
+
+message = " ".join(chr(l) for l in messageord)
+print(message)
+
 
 
 
@@ -101,26 +140,40 @@ length primes = length(primeslist)
 Choose random integers p, q from the list {2, · · · , length primes}
 /* Alice selects two random primes next. */
 
-prime1 = primeslist(p) and prime2 = primeslist(q)
 
-/* Next task is to find the least common multiple of (prime1 − 1) (prime2 − 1)*/
-(g, s) =gcd(prime1 − 1, prime2 − 1)
-λ = (prime1 − 1) (prime2 − 1)/g /* LCM(a, b) = a b
-GCD(a,b)
-*/
+1. )      prime1 = primeslist(p) and prime2 = primeslist(q)
+
+2. )  Next task is to find the least common multiple of (prime1 − 1) (prime2 − 1)*/
+
+3.  ) (g, s) =gcd(prime1 − 1, prime2 − 1)
+
+4.  )  λ = (prime1 − 1) (prime2 − 1)/g 
+
+
+/* LCM(a, b) = a b / GCD(a,b) */
+
+
 found =False
 while found is False do
 Choose a random integer e between 3 and λ
 (k, found) =inverse modulo n(e, λ)
 N = prime1 × prime2
+
+
 /* (N, e) is the public key Alice communicates to Bob. k is her private key.
 */
+
+
 /* Bob selects a message m (through user input) which is a string of
 characters. Bob encodes each character using the formula */
+
+
 s(i) = int(m(i))e mod N /* MatLab: Use the mod command. Python: Use the %
 command. */
 /* MatLab: use (double(m(i)))e. For example, double(0H0) gives the number 72.
 Python: Use ‘‘ord" to convert characters to integers. */
+
+
 /* Bob concatenates the encoded characters into a message that is sent to
 Alice. */
 /* Alice receives the message and decodes each character using the formula */
