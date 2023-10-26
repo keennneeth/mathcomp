7202210ba739525b4eb6b9766458a57364a6f5d2
kenny <64386216+keennneeth@users.noreply.github.com>
2023-10-25 23:09:38 -0500

    Update ECC.py

--- ECC.py
+++ ECC.py
@@ -1,3 +1,6 @@
+import random as rd
+import math
+
 '''
 Need large prime - p
 Elliptic curve - E
@@ -5,27 +8,35 @@ point - P
 ^solution to the elliptic curve equation modulo p
 '''
 
+
+
+'''
+FINDING N + P
+'''
 class ecc(object):
     def __init__(self, x,y):
         self.x = x
         self.y = y
+
     
     def display_field(self):
         print('x = ', self.x, 'y = ', self.y)
 
+
 #ellipitic curve result y^2 = x^3 + ax + b mod p
-def EllipticCurveRoots(a,b,p,x,y):
+def generate(a,b,p):
     i = p-1
     fieldlist = []
-    fieldlist.append(0)
+    fieldlist.append((0,0))
+    print(fieldlist)
     length_list = 1
-    y = pow(i,2)%p
-    math2 = pow(x,3)+(a*x)+b%p
+    # y = pow(i,2)%p
+    # math2 = pow(x,3)+(a*x)+b%p
     for x in range(p-1):
         for i in range(p-1):
-            if y == math2:
+            if (pow(i,2)%p == pow(x,3)+(a*x)+b%p):
                 length_list += 1
-                fieldlist.append(x,i)
+                fieldlist.append((x,i))
     print(fieldlist, length_list)
 
 # def add_fields():
@@ -41,10 +52,20 @@ def EllipticCurveRoots(a,b,p,x,y):
 #         lamb = (y2-y1)/(x2-x1)
 #         nu = (y1*x2) - (y2*x1) / (x2-x1)
 
-#Algo to computer nxP
-def scalarproduct(e):
-    e = bin(e)[2:]
-    print(e)
+'''
+END OF N + P
+'''
+
+
+
+
+
+'''
+algo for nxP
+'''
+# def scalarproduct(e):
+#     e = bin(e)[2:]      #turning into binary number
+#     print(e)
     # for i in range(L-1):
     #     R = m
     #     y = inf
@@ -57,4 +78,21 @@ if __name__ == "__main__":
     # x=int(input("X = : "))
     # y=int(input("Y = : "))
     # EllipticCurveRoots(x,y,40,60,80)
-    scalarproduct(3)
+    p = int(rd.choice(open("primes.txt").readlines()))
+    # x1 = rd.randint(0, 9)
+    # x2 = rd.randint(0, 9)
+    # y1 = rd.randint(0, 9)
+    # y2 = rd.randint(0, 9)
+    myObj = ecc(0,9)
+    generate(myObj.x, myObj.y,p)
+
+
+
+
+
+    '''
+    Alice and Bob agree on a large prime p, 
+    an elliptic curve E, 
+    and a point P which is a solution
+    to the elliptic curve equation modulo p.
+    '''
