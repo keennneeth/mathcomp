8b609ff6d954bc05016489b2a9473a58c4dc8f9a
kenny <64386216+keennneeth@users.noreply.github.com>
2023-11-13 23:44:21 -0600

    Update ECC.py

--- ECC.py
+++ ECC.py
@@ -1,132 +1,90 @@
 import random as rd
 import math
 
-'''
-Need large prime - p
-Elliptic curve - E
-point - P
-^solution to the elliptic curve equation modulo p
-'''
-
-
-
-'''
-FINDING N + P
-'''
 class ecc(object):
-    def __init__(self, x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def display_field(self):
         print('x = ', self.x, 'y = ', self.y)
 
-
-'''
-Algo To find Elliptic Curve Roots
-'''
-#ellipitic curve result y^2 = x^3 + ax + b mod p
-def generate(a,b,p):
+def generate(a, b, p, ordval):
     temp = []
     length_list = 0
     for x in range(p):
         for i in range(p):
-            if (pow(i,2))%p == ((pow(x,3))+(a*x)+b)%p:
+            if (pow(i, 2)) % p == ((pow(x, 3)) + (a * x) + b) % p:
                 length_list += 1
-                temp.append((x,i))
-    print("Field List: ", temp,"\nRoots: ", length_list)
-    # P, Q = (rd.sample(temp, 2))
-    P,Q = (3,4), (5,1)
-    print("P1 :", P,"\nP2 :",Q)
+                temp.append((x, i))
+    print("Field List: ", temp, "\nRoots: ", length_list)
+    P, Q = (5274,2841), (8669,740)
+    print("P1 :", P, "\nP2 :", Q)
+    for i in ordval:
+        print([i], temp[i])
 
-    return P, Q
+    return P, Q, temp
 
-def add_fields(a,b,p):
+def add_fields(P, Q, a, b, p):
+    X1, Y1 = P
+    X2, Y2 = Q
 
-    X1 = P[0]
-    Y1 = P[1]
+    if P == (0,0):
+        return Q
+    if Q == (0,0):
+        return P
 
-    X2 = Q[0]
-    Y2 = Q[1]
-    
-    if X2 - X1 == 0:
-            print("Division by 0 // CANNOT CONTINUE")
-            exit()
+    if X1 == X2:
+        if (Y1 + Y2) % p == 0:
+            return (0,0)
+        else:
+            slope = ((3 * X1**2 + a) * pow(2 * Y1, -1, p)) % p
     else:
-        slope = (Y2 - Y1)/(X2 - X1)
-        # print("Slope = ", slope)
-
-
-    #Rule 1 - if P1 ̸= P2 and x1 = x2, then P1 ⊕ P2 = O
-    if P != Q and X1 == X2:
-        print("R1 - Elliptic Curve Addition: O - Infinite")
-
-    #Rule 2 - If P1 = P2 and y1 = 0, then P1 ⊕ P2 = 2P1 = O
-    elif P == Q and Y1 == 0:
-        print("R2 - Elliptic Curve Addition: O - Infinite")
-
-
-    #Rule 3 - If P1 ̸= P2 (and x1 ̸= x2), let λ and ν = x
-    elif P != Q:
+        # Slope calculation for different points
         slope = ((Y2 - Y1) * pow(X2 - X1, -1, p)) % p
-
-
-    else:
-        slope = ((pow(X1, 3) + a) * pow(2 * Y1, -1, p)) % p
 
     X3 = (pow(slope, 2) - X1 - X2) % p
     Y3 = (slope * (X1 - X3) - Y1) % p
     base = (X3, Y3)
-    print("Elliptic Curve Addition Rules =",base)
-
-     
     return base
 
+def double_point(P, a, p):
+    if P == (0,0):
+        return P
+    else:
+        return add_fields(P, P, a, 0, p)
 
-    # if (i[-1]) == 0:
-    #     fieldlist.append((x,i))
-    # else:
-    #     fieldlist.append(temp[::2])
-    # print(fieldlist)
-    # fieldlist.append(temp[::2])
-   
+def nXp(m, e, a, b, p):
+    y = (0,0)
+    binary = bin(e)[2:]
+    for bit in binary:
+        y = double_point(y, a, p)
+        if bit != '0':
+            y = add_fields(y, m, a, b, p)
 
-'''
-END
-'''
+    return y
 
-
-
-'''
-END OF N + P
-'''
-
-
-
-'''
-algo for nxP
-P being the Base in add_fields
-'''
-
-
-
-   
+def ordmsg(m):
+    ordmsg = []
+    for i in m:
+        ordmsg1 = ord(i)
+        ordmsg.append(ordmsg1)
+    return ordmsg
 
 if __name__ == "__main__":
-    #ellipitic curve result y^2 = x^3 + ax + b mod p
-    # A // B // P
-    num = rd.randint(1, 50)
+    num = 10
     e = bin(num)[2:]
-    m = "Test"
-    P, Q = generate(3, 8, 7)
-    #a, b, p
-    base = add_fields(3, 8, 7)
+    m = "testing"
+    ordval = ordmsg(m)
+    P, Q, temp = generate(3, 8, 100, ordval)
 
+    Pmult = 25
+    Qmult = 34
 
+    base = add_fields(P, Q, 497, 1768, 9739)
+    result_5P = nXp(P, Pmult, 497, 1768, 9739)
+    result_2Q = nXp(Q, Qmult, 497, 1768, 9739)
 
-    '''
-    Alice and Bob agree on a large prime p, 
-    an elliptic curve E, 
-    and a point P which is a solution
-    to the elliptic curve equation modulo p.
-    '''
+    print(Pmult,"P",result_5P)
+    print(Qmult,"Q",result_2Q)
+    print("Elliptic Curve Addition - ", base)
