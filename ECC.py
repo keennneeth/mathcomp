9f8932a3af28b4363c6fb2ad5c39ed1718785f5f
kenny <64386216+keennneeth@users.noreply.github.com>
2023-11-18 23:17:48 -0600

    Update ECC.py

--- ECC.py
+++ ECC.py
@@ -89,13 +89,65 @@ def ordmsg(m):
 def ECC(base, ordval,temp):
     # nA = rd.choice(50)
     print("Message -",m)
-
     print("\n--------")
     EncodedM = []
-
     for i in ordval:
         EncodedM.append(temp[i])
-    print("Encoded Message - ",EncodedM)
+    return EncodedM
+
+
+'''
+(1) Alice and Bob agree on a large prime p, an elliptic curve E, and a point P which is a solution
+to the elliptic curve equation modulo p.
+(2) Alice chooses a private integer nA, computes QA = nA P, and sends QA to Bob.
+(3) Bob chooses a large private integer nB, computes QB = nB P and sends QB to Alice.
+(4) Assume Bob is the sender. Bob chooses a message m, which is a string. Bob encodes it using
+a cipher into M, which is a list of roots of the elliptic curve E.
+(5) Bob chooses a random ephemeral key e which is to be used for only one message and thrown
+away.
+(6) Bob computes the two quantities c1 = e P, which is a number, and the list of numbers
+c2 = M + k QA. Bob sends (c1, c2) to Alice.
+(7) Alice decrypts the message by computing (c2 − nA c1.)
+'''
+def mainstuff(P, e, a, b, p, ordval, EncodedM):
+
+    nA = rd.choice(range(0,100))
+    nB = rd.choice(range(0,100))
+   
+    QA = nXp(P, nA, a,b,p)
+    QB = nXp(P, nB, a,b,p)
+    
+    c1 = nXp(P, e, a, b, p)
+    #2
+    print("Alice's Private Integer -", nA)
+    #3
+    print("Bob's Private Integer -", nB)
+    print("Alice's QA -", QA)
+    print("Bob's QB -", QB)
+
+    #4
+    print("Bobs Encoded message (list of roots) - ", EncodedM)
+    #5
+    print("Bob's random ephemeral key e -", e)
+    print("Bob computes c1 (eP) -", c1)
+
+    c2 = []
+    for i in EncodedM:
+        c2.append(add_fields(i, c1, a,b,p))
+    print("c2 - ", c2)
+
+    print("Alice recieves the 2 quantites", c1, "and", c2)
+
+    nAc1 = nXp(c1, nA, a,b,p)
+    print("Alice computes nAc1 -", nAc1)
+
+
+    c2_m_nAc1 = []
+    for i in c2:
+        c2_m_nAc1.append(i[0] - nAc1[0])
+    print("Alice decrypts the message using c2 - nAc1", c2_m_nAc1)
+
+
 
 
 
@@ -105,7 +157,7 @@ def ECC(base, ordval,temp):
 
 
 if __name__ == "__main__":
-    e = 10
+    e = rd.choice(range(0,20))
     m = "testing"
     ordval = ordmsg(m)
     a = 3
@@ -120,9 +172,11 @@ if __name__ == "__main__":
     result_5P = nXp(P, Pmult, a,b,p)
     result_2Q = nXp(Q, Qmult, a,b,p)
 
-    test = ECC(base, ordval, temp)
+    EncodedM = ECC(base, ordval, temp)
+
+    main = mainstuff(P, e, a, b, p, ordval, EncodedM)
 
 
-    print(Pmult,"P",result_5P)
-    print(Qmult,"Q",result_2Q)
-    print("Elliptic Curve Addition - ", base)
+    # print(Pmult,"P",result_5P)
+    # print(Qmult,"Q",result_2Q)
+    # print("Elliptic Curve Addition - ", base)
