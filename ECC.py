c22af3485a9cc0073a66e281e6fc210f8850b92a
kenny <64386216+keennneeth@users.noreply.github.com>
2023-11-27 22:59:49 -0600

    Update ECC.py

--- ECC.py
+++ ECC.py
@@ -42,21 +42,22 @@ def add_fields(P, Q, a, b, p):      #Algprithm to compute P + Q (Addition)
             return (0, 0)
         slope = ((Y2 - Y1) * pow(X2 - X1, -1, p)) % p
 
-    X3 = (pow(slope, 2) - X1 - X2) 
-    Y3 = (slope * (X1 - X3) - Y1) 
+    X3 = (pow(slope, 2) - X1 - X2) % p
+    Y3 = (slope * (X1 - X3) - Y1) % p
     base = (X3, Y3)
     return base
 
 
 def nXp(m, e, a, b, p):     #Algorithm to compute nXP
-    R = m
-    y = (0,0)
+    R = (0, 0)
     binary = bin(e)[2:]
+
     for bit in binary:
-        if bit != '0':
-            y = add_fields(y, m, a, b, p)
-        R = add_fields(R, m, a, b, p)
-    return y
+        R = add_fields(R, R, a, b, p)
+        if bit == '1':
+            R = add_fields(R, m, a, b, p)
+
+    return R
 
 def ordmsg(m):
     ordmsg = []
@@ -79,110 +80,132 @@ def ECC(base, ordval,fieldlist):
     return EncodedM
 
 
-# def decrypt(c2, fieldlist, EncodedM, m, P, a, b, p, ordval):
-#     original_message = m
-#     e = rd.choice(range(150, 200))
-#     while c2 != EncodedM:
-#         e = rd.choice(range(150, 200))
-#         c2 = mainstuff(P, e, a, b, p, ordval, EncodedM)
-#Just in case the decryption is encorrect, it will reset and try again.
-
-
-    print("decrypt c2 - ", c2)
-
-    c = [fieldlist.index(x) for x in c2 if x in c2 and x in fieldlist]
-    print(c)
-    decryptedmessage = ''.join(chr(num) for num in c)
-
-    print(decryptedmessage)
-
-'''
-(1) Alice and Bob agree on a large prime p, an elliptic curve E, and a point P which is a solution
-to the elliptic curve equation modulo p.
-(2) Alice chooses a private integer nA, computes QA = nA P, and sends QA to Bob.
-(3) Bob chooses a large private integer nB, computes QB = nB P and sends QB to Alice.
-(4) Assume Bob is the sender. Bob chooses a message m, which is a string. Bob encodes it using
-a cipher into M, which is a list of roots of the elliptic curve E.
-(5) Bob chooses a random ephemeral key e which is to be used for only one message and thrown
-away.
-(6) Bob computes the two quantities c1 = e P, which is a number, and the list of numbers
-c2 = M + k QA. Bob sends (c1, c2) to Alice.
-(7) Alice decrypts the message by computing (c2 − nA c1.)
-'''
-def mainstuff(P, e, a, b, p, ordval, EncodedM):
-
-    # nA = rd.choice(range(0,100))
-    # nB = rd.choice(range(0,100))
-    nA = 5
-    nB = 7
-
-
-    '''
-    P  = 3,4
-    '''
-   
-    QA = nXp(P, nA, a,b,p)
-    QB = nXp(P, nB, a,b,p)
-    
-    c1 = nXp(P, e, a, b, p)
-    #2
-    print("Alice's Private Integer -", nA)
-    #3
-    print("Bob's Private Integer -", nB)
-    print("Alice's QA -", QA)
-    print("Bob's QB -", QB)
-
-    #4
-    print("Bobs Encoded message (list of roots) - ", EncodedM)
-    #5
-    print("Bob's random ephemeral key e -", e)
-    print("Bob computes c1 (eP) -", c1)
-
-    # print("Alice recieves the 2 quantites", c1, "and", c2)
-
-    nAc1 = nXp(c1, nA, a,b,p)
-    print("Alice computes nAc1 -", nAc1)
-    c2 = [add_fields(m, nXp(QA, e, a, b, p), a, b, p) for m in EncodedM]
-
+def calculate_c2(M, k, QA, a, b, p):
+    kQA = nXp(QA, k, a, b, p)
+    c2 = [add_fields(m, kQA, a, b, p) for m in M]
     return c2
 
+def negate(point, p):
+    # Negate the y-coordinate of the point in the finite field
+    x, y = point
+    negated_point = (x, (-y) % p)
+    return negated_point
 
-    # c2_m_nAc1 = []
-    # for i in c2:
-    #     c2_m_nAc1.append((i[0] - nAc1[0], i[1] - nAc1[1]))
-    # print("Alice decrypts the message using c2 - nAc1", c2_m_nAc1)
+
+def mainstuff(P, a, b, p, EncodedM):
+
+    '''
+    (1) Alice and Bob agree on a large prime p, an elliptic curve E, and a point P which is a solution
+    to the elliptic curve equation modulo p.
+    '''
+
+    '''
+    (2) Alice chooses a private integer nA, computes QA = nA P, and sends QA to Bob.
+    '''
+    # nA = rd.choice(range(0,100))
+    nA = 1194
+    QA = nXp(P, nA, a,b,p)
+    print("QA = ", QA)
+
+    '''
+    (3) Bob chooses a large private integer nB, computes QB = nB P and sends QB to Alice.
+    '''
+    # nA = rd.choice(range(0,100))
+    nB = 1759
+    QB = nXp(P, nB, a,b,p)
+    print("QB = ", QB)
+
+
+    '''
+    (4) Assume Bob is the sender. Bob chooses a message m, which is a string. Bob encodes it using
+    a cipher into M, which is a list of roots of the elliptic curve E
+    '''
+    print("Bobs Encoded message (list of roots) - ", EncodedM)
 
 
 
-    # # Step 7: Alice decrypts the message by computing c2 - nA * c1
-    # nAc1 = nXp(c1, nA, a, b, p)
-    # decrypted_message = [add_fields(m, nAc1, a, b, p) for m in c2]
-    # print("Testing - ", decrypted_message)
 
+    '''
+   (5) Bob chooses a random ephemeral key e which is to be used for only one message and thrown
+    away.
+    '''
+    e = rd.choice(range(0,20))
+    # e = 10
+    print("e in main = ", e)
+
+
+    '''
+    (6) Bob computes the two quantities c1 = e P, which is a number, and the list of numbers
+    c2 = M + k QA. Bob sends (c1, c2) to Alice.
+    '''
+
+    c1 = nXp(P, e, a,b,p)
+    print("C1 = ", c1)
+
+
+    c2 = calculate_c2(EncodedM, e, QA, a, b, p)
+    print("C2 = ", c2)
+
+    '''
+    (7) Alice decrypts the message by computing (c2 − nA c1)
+    '''
+    nAc1 = nXp(c1, nA, a,b,p)
+    print("nAc1 = ", nAc1)
+
+
+
+    decrypted = []
+    for point in c2:
+        decrypted_point = add_fields(point, negate(nAc1, p), a, b, p)
+        decrypted.append(decrypted_point)
+    print("Decrypted Values = ", decrypted)
+
+    # Find the indices of the decrypted points in the fieldlist
+    indices = [fieldlist.index(point) for point in decrypted]
+
+    # Print the ASCII values corresponding to the indices
+    ascii_values = [point[0] for point in fieldlist]
+    decrypted_ascii = [ascii_values[index] for index in indices]
+
+    print("Indices of points in fieldlist:", indices)
+
+    decrypyedmsg = []
+    for i in indices:
+        decrypyedmsg.append(chr(i))
+    print(decrypyedmsg)
+
+    string = ''.join(str(j) for j in decrypyedmsg)
+    print("DECRYPTED MESSAGE = ", string)
 if __name__ == "__main__":
-    # e = rd.choice(range(0,20))
-   
-    e = 32
-    m = "Hello World"
-    ordval = ordmsg(m)
-    a = 3
-    b = 8
-    p = 7
 
-    fieldlist, length_list= generate(a,b,200, ordval,m)
-    P, Q = (3,4), (5,1)
+    m = "i am testing my code, WORKING!!!!"
+    ordval = ordmsg(m)
+    a = 324
+    b = 1287
+    p = 3851
+
+    # a = 3
+    # b = 8
+    # p = 7
+
+    fieldlist, length_list= generate(a,b,p, ordval,m)
+    # P, Q = (5274, 2841), (8669, 740)
+    P, Q = (920, 303), (5,1)
     #P, Q = (rd.sample(fieldlist, 2))
 
     Pmult = 5
     Qmult = 2
 
     base = add_fields(P, Q, a,b,p)
-    result_5P = nXp(P, Pmult, a,b,p)
-    result_2Q = nXp(Q, Qmult, a,b,p)
+    print("BASE = ",base)
+    # result_5P = nXp(P, Pmult, a,b,p)
+    # print("5P Result - ", result_5P)
+    result_2Q = nXp(P, Qmult, a,b,p)
+    print("2P Result - ", result_2Q)
 
     EncodedM = ECC(base, ordval, fieldlist)
 
-    c2 = mainstuff(P, e, a, b, p, ordval, EncodedM)
+    c2 = mainstuff(P, a, b, p, EncodedM)
 
     # origmessage = decrypt(c2, fieldlist, EncodedM, m, P, a, b, p, ordval)
     # print(Pmult,"P",result_5P)
